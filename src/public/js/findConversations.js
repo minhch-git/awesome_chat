ad1a3fed2f5d50acf162252ec6c7efaecc277c27
minhchiu <minhch.vn@gmail.com>
2021-09-03 18:49:59 +0700

    [1.0.0] search conversation and add modal list members info

---
+++ src/public/js/findConversations.js
@@ -0,0 +1,66 @@
+function findConversations() {
+  $("input.searchBox").on("input", function (e) {
+    let keyword = $(this).val();
+    if (!keyword) {
+      $("#search-results").css("display", "none");
+      return;
+    }
+  });
+
+  $("input.searchBox")
+    .unbind("keyup")
+    .on("keyup", function (e) {
+      if (e.which == 13) {
+        let keyword = $(this).val();
+        fetch(`/find-conversations/${keyword}`)
+          .then(response => {
+            if (!response.ok) throw response.json();
+            return response.json();
+          })
+          .then(data => renderConversation(data))
+          .catch(async ex => {
+            const errors = await ex;
+            $("#search-results").css("display", "block");
+            $("#search-results")
+              .find(".search_content ul")
+              .empty()
+              .append(`<li>${errors.message}</li>`);
+          });
+      }
+    });
+}
+
+function renderConversation(conversations) {
+  $("#search-results").css("display", "block");
+
+  let itemRender = conversations.map(conversation => {
+    let avatarSrc = conversation.avatar
+      ? `images/users/${conversation.avatar}`
+      : "images/users/group-avatar-trungquandev.png";
+    return `
+      <li data-chat="${conversation._id}">
+        <img src="${avatarSrc}" /> ${
+      conversation?.name || conversation?.username
+    }
+      </li>
+    `;
+  });
+  $("#search-results").find(".search_content ul").empty().append(itemRender);
+  showChat();
+}
+
+function showChat() {
+  $("#search-results")
+    .find("li")
+    .unbind("click")
+    .on("click", function (e) {
+      let targetId = $(this).data("chat");
+      $("ul.people").find(`a[data-target="#to_${targetId}"]`).click();
+      $("#search-results").css("display", "none");
+      $("input.searchBox").val("");
+    });
+}
+
+$(document).ready(() => {
+  findConversations();
+});
--- src/routes/siteRoute.js
+++ src/routes/siteRoute.js
@@ -1,15 +1,17 @@
 // import controller
-import siteController from '../app/controllers/SiteController'
-import isLogin from './../validation/isLogin'
+import siteController from "../app/controllers/SiteController";
+import isLogin from "./../validation/isLogin";
 
-import { Router } from 'express'
-const router = new Router()
+import { Router } from "express";
+const router = new Router();
+
+router.get("/find-conversations/:keyword", siteController.findConversation);
 
 router.get(
-  '/login-register',
+  "/login-register",
   isLogin.isLoggedOut,
   siteController.getLoginRegister
-)
-router.get('/', isLogin.isLoggedIn, siteController.getHome)
+);
+router.get("/", isLogin.isLoggedIn, siteController.getHome);
 
-export default router
+export default router;
